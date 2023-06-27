d0cd1cf852d831da8711c23f10b91ae53436b137
David Antonio Arenas Islas <davidantarenas@icloud.com>
2023-06-26 20:36:36 -0600

    acces localhost

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 import express from "express";
+import cors from "cors";
 import usersPersonalRoutes from "./routes/users_personal.routes.js";
-import indexRoutes from "./routes/index.routes.js"
+import indexRoutes from "./routes/index.routes.js";
 
-import './config.js'
+import './config.js';
 
 const app = express();
 
-app.use(express.json())
+// Configurar CORS
+app.use(cors());
+
+app.use(express.json());
 
 app.use(indexRoutes);
 
 app.use('/api', usersPersonalRoutes);
 
-app.use((req, res, next) =>{
-    res.status(404).json({
-        message:'endpoint not found'
-    })
-})
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Endpoint not found'
+  });
+});
 
-export default app
+export default app;
