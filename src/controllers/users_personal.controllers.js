5844c91f1b755b19416480082985d517ede269c8
David Antonio Arenas Islas <davidantarenas@icloud.com>
2023-06-29 18:18:09 -0600

    Usuario Contraseña

--- src/controllers/users_personal.controllers.js
+++ src/controllers/users_personal.controllers.js
@@ -19,63 +19,57 @@ export const getUserPersonalById = async (req, res) => {
 };
 
 export const createUserPersonal = async (req, res) => {
-  const { user, password } = req.body;
+  const { Usuario, Contraseña } = req.body;
   const [rows] = await pool.query(
-    "INSERT INTO users_personal (user, password) VALUES (?,?)",
-    [user, password]
+    "INSERT INTO users_personal (Usuario, Contraseña) VALUES (?,?)",
+    [Usuario, Contraseña]
   );
   console.log(req.body);
   res.send({
     id: rows.insertId,
-    user,
-    password,
+    Usuario,
+    Contraseña,
   });
 };
 
 export const deleteUserPersonal = async (req, res) => {
-  const [result] = await pool.query("DELETE FROM users_personal WHERE id = ?", [
-    req.params.id,
-  ]);
+  const [result] = await pool.query(
+    "DELETE FROM users_personal WHERE id = ?", [req.params.id]
+  )
   if (result.affectedRows <= 0)
-    return res.status(404).json({
-      message: "User not found",
-    });
+  return res.status(404).json({
+    message: "User not found",
+  })
 
-  res.sendStatus(204);
-};
+res.sendStatus(204);
+}
 
 export const updateUserPersonal = async (req, res) => {
-  const { id } = req.params;
-  const { user, password } = req.body;
+    const {id} = req.params
+    const {Usuario, Contraseña} = req.body
+    
+    const [result] = await pool.query('UPDATE users_personal SET Usuario = IFNULL(?, Usuario), Contraseña = IFNULL(?, Contraseña) WHERE id = ?', [Usuario, Contraseña, id])
 
-  const [result] = await pool.query(
-    "UPDATE users_personal SET user = IFNULL(?, user), password = IFNULL(?, password) WHERE id = ?",
-    [user, password, id]
-  );
+    if (result.affectedRows === 0) return res.status(404).json({
+        message: "User not found"
+    })
 
-  if (result.affectedRows === 0)
-    return res.status(404).json({
-      message: "User not found",
-    });
+    const [rows] = await pool.query('SELECT * FROM users_personal WHERE id = ?', [id])
 
-  const [rows] = await pool.query("SELECT * FROM users_personal WHERE id = ?", [
-    id,
-  ]);
-
-  res.json(rows[0]);
-};
+    res.json(rows[0])
+}
 
 export const validateUserPersonal = async (req, res) => {
-  const { user, password } = req.body;
+  const { Usuario, Contraseña } = req.body;
 
   const [rows] = await pool.query(
-    "SELECT * FROM users_personal WHERE user = ? AND password = ?",
-    [user, password]
+    "SELECT * FROM users_personal WHERE Usuario = ? AND Contraseña = ?",
+    [Usuario, Contraseña]
   );
 
   if (rows.length <= 0) {
     return res.status(401).json({
-      message: "Invalid username or password",
+      message: "Invalid username or Contraseña",
     });
   }
 
